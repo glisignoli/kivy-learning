16554cb17d90a9b934f4e298f1eea026e8dec32b
glisignoli <glisignoli@gmail.com>
2016-11-19 18:08:25 +1300

    Add kivy version requrirements

--- 00-lable.py
+++ 00-lable.py
@@ -1,12 +1,10 @@
 import kivy
-kivy.require('1.9.1') # replace with your current kivy version !
-
 from kivy.app import App
 from kivy.uix.label import Label
+kivy.require('1.9.1')
 
 
 class MyApp(App):
-
     def build(self):
         return Label(text='Hello world')
 
