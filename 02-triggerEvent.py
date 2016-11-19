0336e3feba26ea64a5fce3218fc9912467b8da80
glisignoli <glisignoli@gmail.com>
2016-11-19 13:37:56 +1300

    Finished a trigger event example

--- 02-triggerEvent.py
+++ 02-triggerEvent.py
@@ -1,50 +1,56 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 from kivy.clock import Clock
+from kivy.uix.gridlayout import GridLayout
 
-class MyWidget(App):
-    """Simple widget that will trigger a popup after pressing a button
+
+class MyGrid(GridLayout):
+    """ Create a gridlayout that contains a button and a label
+    When the button is pressed, use it to schedule an event (trigger)
+    that will update the label's counter by 1
     """
-    def build(self):
-        # Create the button
-        launch_button = Button(text = 'Open Popup')
-        launch_button.bind(on_press=create_popup)
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 2
 
-        return launch_button
-
-    def create_popup(self, dt):
-        #foo = MyPopup()
-        #foo.open()
+        label1 = MyLabel()
 
         # Create the trigger
-        my_trigger = Clock.create_trigger(MyPopup())
-        my_trigger()
+        trigger = Clock.create_trigger(label1.update_counter, 1)
 
+        # Create the button, and on press call the trigger
+        button1 = Button(text="Press me!")
+        button1.bind(on_press=trigger)
 
-class MyPopup(Popup):
-    """Popup with a single button that will close itself
+        # Add the widgets to the gridlayout
+        self.add_widget(button1)
+        self.add_widget(label1)
+
+class MyLabel(Label):
+    """ This label contains an internal counter that can be updated by calling update_counter
     """
+    counter = 0
 
     def __init__(self, **kwargs):
-        """ Initalise the label to update twice ever second
+        """ Initally create the counter text
         """
-        super(MyPopup, self).__init__(**kwargs)
+        super(MyLabel, self).__init__(**kwargs)
+        self.text = 'The counter is now: ' + str(self.counter)
 
-        self.title = 'Hello'
-
-        content = Button(text='Close me!')
-        content.bind(on_press=self.dismiss)
-
-        self.add_widget(content)
+    def update_counter(self, dt):
+        """ Update counter function that can be called via a trigger or schedule
+        """
+        self.counter += 1
+        self.text = 'The counter is now: ' + str(self.counter)
 
 
 class TriggerEvent(App):
     """ Main class that will build
     """
     def build(self):
-        return MyWidget
+        return MyGrid()
 
 
 if __name__ == '__main__':
-    MyWidget().run()
+    TriggerEvent().run()
